17680ec8ae19784ff421b2fd60b560f56579e7dd
Burak Surmen <busurmen@gmail.com>
2020-10-05 17:47:41 +0300

    Created failure component

--- src/components/Failure.vue
+++ src/components/Failure.vue
@@ -1,7 +1,51 @@
 <template>
-  <div>
-    Failure
+  <div class="container">
+    <h1>You failed!!!</h1>
+    <button @click="newGame">New Game</button>
   </div>
 </template>
 
-<style></style>
+<script>
+export default {
+  methods: {
+    newGame() {},
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: darkred;
+}
+.container h1 {
+  text-align: center;
+  font-size: 333%;
+  color: white;
+}
+.container button {
+  width: 200px;
+  background-color: #fff;
+  color: darkred;
+  border: 1px solid brown;
+  height: 50px;
+  font-size: 15px;
+  border-radius: 5px;
+  outline: none;
+  transition: all 0.5s;
+}
+.container button:hover {
+  background-color: lightgreen;
+  color: #fff;
+  transition: all 0.5s;
+  cursor: pointer;
+}
+</style>
